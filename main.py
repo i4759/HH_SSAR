fd786e1ab2b1cdde32efa3cb68e5fcf4e6b78c5f
i4759 <i4759@icloud.com>
2025-08-18 02:38:59 +0300

    added UltimateMathcer menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import word2vec
 import HH
+import UltimateMatchingModel
 
 from pick import pick
 
@@ -7,7 +8,7 @@ if __name__ == "__main__":
     action = ''
 
     while action != 'Выход':
-        select_mode = ['HH', 'Word2Vec', 'Выход']
+        select_mode = ['HH', 'Word2Vec', 'UltimateMatcher', 'Выход']
 
         if action == '':
             action = pick(select_mode, 'Пожалуйста, выберите модуль:', indicator='>')[0]
@@ -16,6 +17,8 @@ if __name__ == "__main__":
             HH.hh_menu()
         elif action == 'Word2Vec':
             word2vec.word2vec_menu()
+        elif action == 'UltimateMatcher':
+            UltimateMatchingModel.ultimate_matching_menu()
         else:
             print("Выход...")
             break
